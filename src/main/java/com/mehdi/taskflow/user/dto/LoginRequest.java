ca5b3536a821c815f2c8e4af40bacd7692de120f
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-03-29 19:02:47 +0200

    feat: add auth DTOs (RegisterRequest, LoginRequest, AuthResponse)

---
+++ src/main/java/com/mehdi/taskflow/user/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.mehdi.taskflow.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
+    private String username;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
+
+    public LoginRequest() {}
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
---
+++ src/main/java/com/mehdi/taskflow/user/dto/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.mehdi.taskflow.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @Email(message = "Email invalide")
+    @NotBlank(message = "L'email est obligatoire")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, message = "Le mot de passe doit faire au moins 8 caractères")
+    private String password;
+
+    public RegisterRequest() {}
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
