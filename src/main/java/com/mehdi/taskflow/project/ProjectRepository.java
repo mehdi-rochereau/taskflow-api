a8b40c3f92760520c9e8975638a3aee601171160
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-03-30 12:37:11 +0200

    feat: add ProjectRepository

---
+++ src/main/java/com/mehdi/taskflow/project/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.mehdi.taskflow.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findByOwnerId(Long ownerId);
+
+    boolean existsByIdAndOwnerId(Long id, Long ownerId);
+}
