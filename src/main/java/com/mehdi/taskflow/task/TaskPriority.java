017df94c8c5f329516dd6f706fd15d8295492807
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-03-31 14:42:02 +0200

    feat: add TaskStatus and TaskPriority enums

---
+++ src/main/java/com/mehdi/taskflow/task/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.mehdi.taskflow.task;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/mehdi/taskflow/task/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.mehdi.taskflow.task;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
